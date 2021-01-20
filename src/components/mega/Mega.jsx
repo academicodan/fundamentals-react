8c86bd846362d1b23f73b3fb4fb684cff69a8dc7
Daniel Ribeiro <academicodan@gmail.com>
2021-01-20 10:45:47 -0300

    Added function gerarNumeros

--- src/components/mega/Mega.jsx
+++ src/components/mega/Mega.jsx
@@ -1,20 +1,18 @@
 import React, { useState } from 'react';
 
-
-const Mega = () => {
+export default (props) => {
   const [numeros, setNumeros] = useState(Array(8).fill(0))
-  
-  function gerarNumeros () {
-    setNumeros([1, 2, 3, 4, 5, 6, 7, 8]);
+
+  function gerarNumeros() {
+    setNumeros([1, 2, 3, 4, 5, 6, 7, 8])
   }
 
-  return (
+  return(
     <div>
       <h3>Mega</h3>
+      {/*.join() permite mostrar um numero apos o outro com o que esta dentro do parentese */}
       <h4>{numeros.join(' ')}</h4>
-      <button onClick={{gerarNumeros}}>Gerar Numeros</button>
+      <button onClick={gerarNumeros}>Gerar Numeros</button>
     </div>
   )
 }
-
-export default Mega
