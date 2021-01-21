683038f202089da17ea655155bce9a639c664dd0
Daniel Ribeiro <academicodan@gmail.com>
2021-01-21 12:05:03 -0300

    Segunda versão

--- src/components/mega/Mega.jsx
+++ src/components/mega/Mega.jsx
@@ -3,8 +3,23 @@ import React, { useState } from 'react';
 export default (props) => {
   const [numeros, setNumeros] = useState(Array(8).fill(0))
 
+  function gerarNumerosNaoContido(array) {
+    const min = 1
+    const max = 60
+    const novoNumero = parseInt(Math.random() * (max - min) + min)
+    return array.includes(novoNumero) ? gerarNumerosNaoContido(array) : novoNumero
+  }
+
   function gerarNumeros() {
-    setNumeros([1, 2, 3, 4, 5, 6, 7, 8])
+    const novoArray = Array(8)
+    .fill(0)
+    .reduce((a, e) => {
+      const novoNumero = gerarNumerosNaoContido(a)
+      console.log(a, e, novoNumero)
+      return[...a, novoNumero]
+    }, [])
+    .sort((a,b) => a - b)
+    setNumeros(novoArray)
   }
 
   return(
